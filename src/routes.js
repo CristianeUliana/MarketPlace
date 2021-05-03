e4260cb42b0391685353c865477985dacc42c93c
Cristiane Goncalves Uliana <cristianeguliana@gmail.com>
2021-05-03 18:27:20 -0300

    Criando e estilizando componentes

--- src/routes.js
+++ src/routes.js
@@ -2,19 +2,27 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Catalogo from './pages/Catalogo';
+import Header from './components/Header';
 
 const Stack = createStackNavigator();
 
 export default function Routes() {
     return(
         <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator
+            screenOptions={{
+                headerShown: true,
+                cardStyle: {backgroundColor: '#313746'},
+            }}
+            initialRouteName="Catalogo"
+            >
                 <Stack.Screen 
                 name='Catalogo'
                 component={Catalogo}
                 options={{
-                    title: 'Catalogo',
-                    headerTitleAlign: 'center',
+                   headerShown: true,
+                   headerTransparent: true,
+                   headerTitle: () => <Header />,
                 }}
                />   
             </Stack.Navigator>
