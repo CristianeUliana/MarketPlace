68edee88c680f8a582e2d8c1c7c3d1909d32f339
Cristiane Goncalves Uliana <cristianeguliana@gmail.com>
2021-05-05 11:31:11 -0300

    simulando api com json-server

--- src/pages/Catalogo/index.js
+++ src/pages/Catalogo/index.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { RecyclerViewBackedScrollViewComponent, View } from 'react-native';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
 import FloatingCart from '../../components/FloatingCart';
 
+import api from '../../services/api';
 import formatValue from '../../utils/formatValue';
 
 import { 
@@ -19,27 +20,18 @@ import {
   ProductButtonText 
 } from './styles'
 
-export default function App() {
-  const [products, setProducts] = useState([
-    {
-      id: '1',
-      title: 'Assinatura Mensal',
-      image_url: 'https://res.cloudinary.com/robertosousa1/image/upload/v1594492578/dio/quarterly_subscription_yjolpc.png',
-      price: 150,
-    },
-    {
-      id: '2',
-      title: 'Assinatura Trimestral',
-      image_url: 'https://res.cloudinary.com/robertosousa1/image/upload/v1594492578/dio/quarterly_subscription_yjolpc.png',
-      price: 250,
-    },
-    {
-      id: '3',
-      title: 'Assinatura Anual',
-      image_url: 'https://res.cloudinary.com/robertosousa1/image/upload/v1594492578/dio/quarterly_subscription_yjolpc.png',
-      price: 1000,
-    },
-  ]);
+export default function Catalog() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function loadProducts() {
+      const { data } = await api.get('/products')
+
+      setProducts(data);
+    }
+    loadProducts()
+  }, []);
+
   return (
     <Container>
       <ProductContainer>
